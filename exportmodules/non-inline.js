91afbc768efa29bba290ef1b8d3acbecf15af191
Emanuel <emanunelzsmj@gmail.com>
2023-04-13 16:59:31 -0300

    Formas de Export do ESModules

---
+++ exportmodules/non-inline.js
@@ -0,0 +1,11 @@
+function group(){
+    console.log("Export nomeado nao inline")
+}
+
+function exportDefault(){
+    console.log('Export default nao-inline')
+}
+
+
+export { group }
+export default  exportDefault 
