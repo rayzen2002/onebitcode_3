29850db010ad33c79c1e7613cf9238b00746d597
Emanuel <emanunelzsmj@gmail.com>
2023-04-14 01:39:24 -0300

    Usando e configurando o webpack + loaders/plugins

---
+++ webpack/src/index.js
@@ -0,0 +1,3 @@
+import { dayjs } from 'dayjs'
+
+alert(`Hoje eh: ${dayjs().format('DD-MM-YYYY')}`)
---
+++ webpack/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+    entry : {
+        main: './src/index.js',
+        hello: './src/hello.js' 
+
+    },
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname , 'public'), 
+        filename: '[name].bundle.min.js'
+
+
+    }
+}
